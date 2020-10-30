e6056a471091df8e85b12e5850fb1b1592f0c41c
David Kennedy <david.kennedy@adhocteam.us>
2020-10-30 17:20:30 -0400

    Add new example and test results

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(() => {
+  const form = document.getElementById("focus-shift");
+  const input = document.getElementById("input-error-5");
+  var newNode = document.createElement("div");
+  newNode.setAttribute("id", "error-msg-5");
+  newNode.setAttribute("class", "ds-c-field__hint");
+  newNode.setAttribute("class", "ds-u-color--error");
+  newNode.setAttribute("role", "alert");
+  newNode.textContent = "Example error message";
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    if (input.value.trim() == "") {
+      console.log("Empty!");
+      input.after(newNode);
+      input.setAttribute("aria-invalid", "true");
+      input.setAttribute("aria-describedby", "error-msg-5");
+      input.classList.add("ds-c-field--error");
+      newNode.setAttribute("tabindex", "-1");
+      newNode.focus();
+    }
+  });
+})();
