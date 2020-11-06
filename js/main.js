784053269894a4de1ad12558e08ed9fa00712447
David Kennedy <david.kennedy@adhocteam.us>
2020-11-06 15:40:45 -0500

    Use older ES2015 syntax so this works in IE 11

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-(() => {
-  const form = document.getElementById("focus-shift");
-  const input = document.getElementById("input-error-5");
+(function () {
+  var form = document.getElementById("focus-shift");
+  var input = document.getElementById("input-error-5");
   var newNode = document.createElement("div");
   newNode.setAttribute("id", "error-msg-5");
   newNode.setAttribute("class", "ds-c-field__hint");
   newNode.setAttribute("class", "ds-u-color--error");
   newNode.setAttribute("role", "alert");
   newNode.textContent = "Example error message";
-  form.addEventListener("submit", (evt) => {
+  form.addEventListener("submit", function (evt) {
     evt.preventDefault();
+
     if (input.value.trim() == "") {
       console.log("Empty!");
       input.after(newNode);
